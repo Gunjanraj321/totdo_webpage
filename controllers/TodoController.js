4d7ce72d488dda3f3fa888c093d9ae3138402d1c
Gunjan Kumar <118360371+Gunjanraj321@users.noreply.github.com>
2024-07-14 12:58:51 +0530

    Added: todoTask delete done with route

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -3,29 +3,40 @@ const Todo = require("../model/Todo");
 const createTodo = async (req, res) => {
   try {
     const { title, description, dateToComplete } = req.body;
-    if(!title || !description || !dateToComplete){
-        return res.status(400).json({message: "Please fill all the fields"});
+    if (!title || !description || !dateToComplete) {
+      return res.status(400).json({ message: "Please fill all the fields" });
     }
     const todo = await Todo.create({
-        title,
-        description,
-        dateToComplete : new Date(dateToComplete),
-        });
-        res.status(201).json({ todo });
+      title,
+      description,
+      dateToComplete: new Date(dateToComplete),
+    });
+    res.status(201).json({ todo });
   } catch (error) {
     console.log(error);
-    res.status(500).json({error: 'Server Error : ', error})
+    res.status(500).json({ error: "Server Error : ", error });
   }
 };
 
 const getTodos = async (req, res) => {
-    try {
-        const todos = await Todo.find();
-        return res.status(200).json({ todos });
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({error: 'Server Error : ', error})
-    }
-}
+  try {
+    const todos = await Todo.find();
+    return res.status(200).json({ todos });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server Error : ", error });
+  }
+};
 
-module.exports = {createTodo, getTodos} ;
+const deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findByIdAndDelete(id);
+    return res.status(200).json({ message: "task deleted succefully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server Error : ", error });
+  }
+};
+
+module.exports = { createTodo, getTodos, deleteTodo };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {createTodo, getTodos} = require('../controllers/TodoController');
+const {createTodo, getTodos, deleteTodo} = require('../controllers/TodoController');
 
 router.post('/todos', createTodo);
 router.get('/todos', getTodos);
+router.delete('/todos/:id', deleteTodo);
 
 module.exports = router;
