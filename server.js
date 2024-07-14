c5fa7c589880abd18ba1d266e02193a45b506ff3
Gunjan Kumar <118360371+Gunjanraj321@users.noreply.github.com>
2024-07-14 12:05:55 +0530

    DB connected with mongodb

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>console.log('MongoDb connection successfull'))
+    .catch(error=>console.log(error));
 
 const PORT = 3000;
 
